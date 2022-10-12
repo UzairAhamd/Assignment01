12abeafdc5bc3476cc6b0cb2f20ea4abc8833b87
UzairAhamd <110417995+UzairAhamd@users.noreply.github.com>
2022-10-12 15:12:41 +0500

    Add files via upload

---
+++ UzairAhmadA1Q2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UzairAhmadA1Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //enter the number
+            Console.WriteLine("Enter the Number: ");
+            int numb = Convert.ToInt32(Console.ReadLine());
+            // call the funciton and print the result
+            checkEvenOdd(numb);
+        }
+        //make the funciton to check if the number is even or odd
+        public static void checkEvenOdd(int getNumb)
+        {
+            //check the evenness of the number 
+            if (getNumb % 2 == 0)
+            {
+                Console.WriteLine("Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd");
+            }
+        }
+    }
+}
