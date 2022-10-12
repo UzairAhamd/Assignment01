3769bbf928ba6e6333c0e89f99a83b4a70840cb0
UzairAhamd <110417995+UzairAhamd@users.noreply.github.com>
2022-10-12 15:11:49 +0500

    Add files via upload

---
+++ UzairAhmadA1Q1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UzairAhmadA1Q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get the user name
+            Console.WriteLine("Enter Your Name: ");
+            string name = Console.ReadLine();
+            //print the user with greeting
+            Console.WriteLine("Hello " + name + "!");
+        }
+    }
+}
